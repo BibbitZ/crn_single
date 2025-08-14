2a770e73b3e3e76087660e0052c05ae9bc9ab492
Bibbitz <hunter.of.shadows@gmail.com>
2025-08-14 07:38:17 -0400

    Update keymap.c
    Removed lock PC from Raise layer, as it can be accessed via default layer with right gui (hold) and L

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -88,7 +88,7 @@ TD(TD_LSFT_CAPS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   		KC_N,    KC_M,  
       KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                  XXXXXXX, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TO(_NUM),
-                                     XXXXXXX, MO(ADJUST), XXXXXXX,     XXXXXXX, _______, RGUI(KC_L)
+                                     XXXXXXX, MO(ADJUST), XXXXXXX,     XXXXXXX, _______, XXXXXXX
   ),
 
   [_LOWER] = LAYOUT_split_3x6_3(
